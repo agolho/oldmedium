24008e2942941cbce4a075084713778fd459e163
Yusuf Bektaş <agolho@gmail.com>
2018-10-25 20:10:36 +0300

    screwing around with firebase

--- js/master.js
+++ js/master.js
@@ -1,3 +1,19 @@
 $( "#darkButton" ).click(function() {
   $( "body" ).toggleClass("paintBlack");
 });
+
+
+//Login authenticate n shit
+var provider = new firebase.auth.GoogleAuthProvider();
+
+$("#loginButton").click(function(){
+  console.log("hello");
+  firebase.auth().signInWithRedirect(provider);
+});
+
+
+
+//retreive data
+var database = firebase.database();
+var authors = database.ref('articles').once('value');
+console.log(authors);
